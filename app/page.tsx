51be55ba542c3e014e33c5fc64c1e166b86b1bae
Marina El Keedy <mhk64@mail.aub.edu>
2024-07-26 16:52:23 +0300

    updating messages

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,39 @@
+"use client";
 import ChatBox from "@/components/ChatBox";
+import { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
 
 export default function Home() {
+  const [messages, setMessages] = useState(msgs);
   return (
-    <div className="flex flex-col h-screen p-2">
-      <h1 className="text-blue-500 text-3xl text-center p-4">Care Bot</h1>
-      <div className="flex flex-col border-gray-400 border rounded-xl flex-1 p-4">
-        <ChatBox />
-        <div className="flex gap-2">
-          <input
-            className="border-gray-600 border px-4 py-2 rounded-xl w-full"
-            placeholder="please enter you question..."
-          />
-          <div className="flex justify-center w-12 h-12 items-center bg-blue-400 text-white p-2 rounded-full cursor-pointer">
-            <FaPaperPlane />
-          </div>
+    <div className="flex flex-col gap-4 h-full max-h-screen overflow-hidden p-2 bg-slate-100">
+      <h1 className="text-blue-500 text-3xl text-center pt-4">Care Bot</h1>
+
+      <ChatBox messages={messages} />
+      <div className="flex gap-2">
+        <input
+          className="border-gray-600 border px-4 py-2 rounded-xl w-full"
+          placeholder="please enter you question..."
+        />
+        <div className="flex justify-center w-12 h-12 items-center bg-blue-400 text-white p-2 rounded-full cursor-pointer">
+          <FaPaperPlane />
         </div>
       </div>
     </div>
   );
 }
+
+const msgs = [
+  {
+    user: "You",
+    text: "How are you?",
+  },
+  {
+    user: "CareBot",
+    text: "Fine!",
+  },
+  {
+    user: "You",
+    text: "What's going on?",
+  },
+];
--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,7 +1,23 @@
-export default function ChatBox() {
+export default function ChatBox({ messages }: { messages: any[] }) {
   return (
-    <div className="flex-1">
-      <div>Hello</div>
+    <div className="flex flex-col gap-4 border-gray-400 border rounded-xl flex-1 p-4 bg-white overflow-auto ">
+      <div className="min-h-[65vh]"></div>
+      {messages?.map((message: any, i: number) => (
+        <div
+          key={i}
+          className={`flex ${message.user === "You" && "justify-end"}`}
+        >
+          <div
+            className={`w-3/5 ${
+              message.user === "You"
+                ? "bg-blue-200 border-blue-400"
+                : "bg-gray-300 border-gray-400"
+            } p-2 border  rounded-md`}
+          >
+            {message.text}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
